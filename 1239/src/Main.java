6bec84e2b013e868c1e3fb02c53eaa32f1bf5476
Guilherme Casimiro <guircasimiro@gmail.com>
2017-03-31 21:31:56 -0300

    comentários inseridos

--- 1239/src/Main.java
+++ 1239/src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Main {
 	
-	static int total = 0;
+	static int total = 0; // Variável que vai armazenar o maior tamanho, no momento, da string encontrada
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -26,12 +26,18 @@ public class Main {
 			total = 0;
 		}
 	}
-	
+	/*
+		Método recursivo ele pega a string original e verifica se em k tentativas ela é um palíndromo
+		Caso não seja, então chama duas recursões, por exemplo se a string original for "testando" e
+		tiver qe chamar recursão, a primeira será para "testand" e a segunda para "estando".
+		
+		OBS: os parâmetros begin e end são totalmente desnecessários, desconsidere-os. 
+	*/
 	public static void palindrome(String word, int k, int begin, int end) {
 		//System.out.println(word + " " + begin + " " + end);
 		if(word.length() < total) return;
-		if(word.length() == 1) {
-			if(total == 0) total = 1;
+		if(word.length() == 1) { // Se a palavra for uma letra ela naturalmente é um palindromo
+			if(total == 0) total = 1; 
 			return;
 		}
 		
@@ -44,7 +50,7 @@ public class Main {
 		while(i <= n) {
 			if(subs_word[i] != subs_word[n]) {
 				if(subs_k > 0) {
-					subs_word[i] = subs_word[n];
+					subs_word[i] = subs_word[n]; //não tem necessidade disso, não afetará em nada.
 					subs_k--;
 				} else {
 					reduce = true;
